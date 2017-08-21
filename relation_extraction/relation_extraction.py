4d2d8193f998c1ed57ace8520ab8ed3585773f2f
kiblawi <kiblawi@wisc.edu>
2017-08-21 11:36:14 -0500

    Initial Commit

---
+++ relation_extraction/relation_extraction.py
@@ -0,0 +1,8 @@
+from structures.sentences import Token
+def main():
+    token1 = Token('bob')
+    token1.print_word()
+
+
+if __name__=="__main__":
+    main()
---
+++ relation_extraction/structures/sentences.py
@@ -0,0 +1,9 @@
+#class objects for sentences and tokens
+
+class Token():
+    def __init__(self,word):
+        self.word = word
+
+    def print_word(self):
+        print(self.word)
+
