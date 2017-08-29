1c6d57cd073543eff5f865127b0ad939dfdf16be
kiblawi <kiblawi@wisc.edu>
2017-08-29 14:01:36 -0500

    Added Dependency Objects and Djikstra

--- relation_extraction/relation_extraction.py
+++ relation_extraction/relation_extraction.py
@@ -3,6 +3,38 @@ import sys
 from lxml import etree
 
 from structures.candidates import Token, Sentence, Dependency
+
+
+def Dijkstra(adj_matrix, source):
+    distance = [+sys.maxint] * len(adj_matrix)  # Unknown distance function from source to v
+    previous = [-1] * len(adj_matrix)  # Previous node in optimal path from source
+    distance[source] = 0  # Distance from source to source
+    unreached = range(len(adj_matrix))  # All nodes in the graph are unoptimized -
+    # print distance
+    # print adj_matrix
+    # print source
+
+    while len(unreached) > 0:  # The main loop
+        u = distance.index(min(distance))  # Get the node closest to the source
+        # print unreached
+        # print u
+        if distance[u] == +sys.maxint:
+            break  # all remaining vertices are inaccessible
+        else:
+            unreached.remove(u)
+            for v in unreached:  # where v has not yet been removed from Q.
+                if adj_matrix[u][v] != '':
+                    alt = distance[u] + 1
+                    if alt < distance[v]:  # Relax (u,v,a)
+                        distance[v] = alt
+                        previous[v] = u
+            distance[u] = +sys.maxint  # Set the distance to u to inf so that it get's ignored in the next iteration
+    return previous
+
+
+
+
+
 def main():
     tree = etree.parse(sys.argv[1])
     root = tree.getroot()
@@ -15,8 +47,11 @@ def main():
         candidate_sentence.set_label('Positive')
         tokens = list(sentence.iter('token'))
         for token in tokens:
+            normalized_ner = None
+            if token.find('NormalizedNER') is not None:
+                normalized_ner = token.find('NormalizedNER').text
             candidate_token = Token(token.get('id'), token.find('word').text, token.find('lemma').text, token.find('CharacterOffsetBegin').text,
-                                    token.find('CharacterOffsetEnd').text, token.find('POS').text, token.find('NER').text, normalized_ner=None)
+                                    token.find('CharacterOffsetEnd').text, token.find('POS').text, token.find('NER').text, normalized_ner)
             candidate_sentence.add_token(candidate_token)
 
 
@@ -31,7 +66,13 @@ def main():
         candidate_sentences.append(candidate_sentence)
     for c in candidate_sentences:
         c.print_sentence()
-        c.print_dependencies()
+        c.build_dependency_matrix()
+        c.print_dependency_matrix()
+        c.print_entities()
+
+
+    prev = Dijkstra(candidate_sentences[3].dependency_matrix,int(candidate_sentences[3].tokens[0].token_id))
+    print(prev)
 
 
 
--- relation_extraction/structures/candidates.py
+++ relation_extraction/structures/candidates.py
@@ -3,7 +3,7 @@
 class Token():
     def __init__(self, token_id, word, lemma, char_begin, char_end, pos, ner, normalized_ner=None):
         '''Constructor for Token objects'''
-        self.token_id = token_id
+        self.token_id = int(token_id)
         self.word = word
         self.lemma = lemma
         self.char_begin = char_begin
@@ -16,14 +16,21 @@ class Token():
         '''Prints the word identified with the Token object'''
         print(self.word)
 
+    def get_token_id(self):
+        return self.token_id
+
+    def get_ner(self):
+        return self.ner
 
 class Sentence():
     def __init__(self,sentence_id):
         '''Constructor for Sentence Object'''
         self.sentence_id=sentence_id
         self.tokens = []
+        self.entities = {}
         self.label = None
         self.dependencies = []
+        self.dependency_matrix = None
 
         #Create root token and initialize to first position
         root = Token('0','ROOT',None, None, None, None, None, None)
@@ -32,6 +39,13 @@ class Sentence():
     def add_token(self,token):
         '''Adds a token to sentence'''
         self.tokens.append(token)
+        if token.get_ner() is not None:
+            if token.get_ner() not in self.entities:
+                self.entities[token.get_ner()]=[]
+            self.entities[token.get_ner()].append(token.token_id)
+
+    def print_entities(self):
+        print(self.entities)
 
     def print_sentence(self):
         '''Prints out the sentence'''
@@ -55,6 +69,19 @@ class Sentence():
         for d in self.dependencies:
             d.print_dependency()
 
+    def build_dependency_matrix(self):
+        self.dependency_matrix = [ [ '' for y in range(len(self.tokens))] for x in range(len(self.tokens) ) ]
+        for dependency in self.dependencies:
+            governor_position = int(dependency.governor_token.get_token_id())
+            dependent_position = int(dependency.dependent_token.get_token_id())
+            type = dependency.get_type()
+            self.dependency_matrix[governor_position][dependent_position]=type
+            # add the reverse only if the slot is empty
+            if (self.dependency_matrix[dependent_position][governor_position] == ""):
+                self.dependency_matrix[dependent_position][governor_position] = "-" + type
+
+    def print_dependency_matrix(self):
+        print(self.dependency_matrix)
 
 class Dependency():
     def __init__(self, type, governor_token, dependent_token):
@@ -64,6 +91,9 @@ class Dependency():
 
     def print_dependency(self):
         print(self.type + '\t' + self.governor_token.word + '\t' + self.dependent_token.word)
+
+    def get_type(self):
+        return self.type
 
 
 
